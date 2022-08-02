33aa857014d632aa12fb76e331306d098387f177
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-08-02 18:02:32 +0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -315,33 +315,7 @@ class TitleState extends MusicBeatState
 
 			new FlxTimer().start(2, function(tmr:FlxTimer)
 			{
-				// Get current version of Kade Engine
-				
-				var http = new haxe.Http("https://raw.githubusercontent.com/KadeDev/Kade-Engine/master/version.downloadMe");
-				var returnedData:Array<String> = [];
-				
-				http.onData = function (data:String)
-				{
-					returnedData[0] = data.substring(0, data.indexOf(';'));
-					returnedData[1] = data.substring(data.indexOf('-'), data.length);
-				  	if (!MainMenuState.kadeEngineVer.contains(returnedData[0].trim()) && !InfoState.leftState && MainMenuState.nightly == "")
-					{
-						trace('outdated lmao! ' + returnedData[0] + ' != ' + MainMenuState.kadeEngineVer);
-						InfoState.needVer = returnedData[0];
-						InfoState.currChanges = returnedData[1];
-						FlxG.switchState(new InfoState());
-					}
-					else
-					{
-						FlxG.switchState(new InfoState());
-					}
-                                        http.request();
-				}
-				
-				http.onError = function (error) {
-				  trace('error: $error');
-				  FlxG.switchState(new MainMenuState()); // fail but we go anyway
-				}
+		        FlxG.switchState(new InfoState());
 			});
 			// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
 		}
